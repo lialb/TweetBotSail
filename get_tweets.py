5f9177108474d7d590b97ee4e91bc6bd87571b60
ElegantDalek <karatedkid@gmail.com>
2019-02-16 18:25:47 -0600

    Tweepy basics working, requests user timeline

---
+++ get_tweets.py
@@ -0,0 +1,17 @@
+import tweepy
+import json
+
+with open('api_keys.json') as f:
+    api_keys = json.load(f)
+
+auth = tweepy.OAuthHandler(api_keys['key'], api_keys['secret'])
+auth.set_access_token(api_keys['access_token'], api_keys['access_secret'])
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
+
+#print(api_keys['key'])
+#print(api_keys['secret'])
