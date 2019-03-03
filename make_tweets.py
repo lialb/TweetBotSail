3377eeaea1bec8f4aafb35f3b6af5c3be15eb571
yiliang6 <yiliang6@illinois.edu>
2019-03-02 22:14:26 -0600

    finished working basic template

--- make_tweets.py
+++ make_tweets.py
@@ -1,15 +1,21 @@
 import tweepy
+import json
 '''
-This class is to make the actual tweets from the AI generated tweets.
+This class is to make the actual tweets from the AI generated tweets. It parses
+through the json containing the keys and access tokens.
 '''
+with open('api_keys_copy.json') as data_file:
+    api_keys = json.load(data_file)
 
 auth = tweepy.OAuthHandler(api_keys['key'], api_keys['secret'])
 auth.set_access_token(api_keys['access_token'], api_keys['access_secret'])
 
-#check if you have the right account info
-user = api.me()
-print (user.name)
-
-
 api = tweepy.API(auth)
-api.update_status (**status** = 'YOUR_TWEET_HERE')
+
+
+#check if you have the right account info
+
+user = api.me()
+print(user.name)
+
+api.update_status('Fake News! Sad!')
